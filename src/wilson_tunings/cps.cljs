a2adaabddbb47b937510e26cf9bf170b41b29210
Diego <diego.vid.eco@gmail.com>
2021-08-01 11:18:29 -0500

    [ADD] table that show ratios and factors for cps

--- src/wilson_tunings/cps.cljs
+++ src/wilson_tunings/cps.cljs
@@ -1,11 +1,13 @@
 (ns wilson-tunings.cps
-  (:require [clojure.string :as str]
-            [erv.cps.core :as cps]
-            [erv.scale.core :as scale]
-            [erv.scale.scl :as scl]
-            [erv.utils.conversions :refer [cps->midi]]
-            [erv.utils.core :as utils]
-            [erv.utils.conversions :as conv]))
+  (:require
+   [com.gfredericks.exact :as e]
+   [clojure.string :as str]
+   [erv.cps.core :as cps]
+   [erv.scale.core :as scale]
+   [erv.scale.scl :as scl]
+   [erv.utils.conversions :refer [cps->midi]]
+   [erv.utils.core :as utils]
+   [erv.utils.conversions :as conv]))
 
 (defn parse-generators [generators]
   (-> generators (str/split #",")
@@ -15,21 +17,73 @@
                         (js/Number.isNaN %)))
            sort)))
 
-(defn make-tidal-scale [set-size generators period]
-  (let [gens (parse-generators generators)
-        scale (->> (cps/make (int set-size) gens :period period :norm-gen
-                             (->> gens (take-last (int set-size)) (apply *)))
-                   :scale)
+(defn get-scale-data [set-size generators period]
+  (let [gens (parse-generators generators)]
+    (cps/make (int set-size) gens :period period
+              :norm-gen (->> gens (take-last (int set-size)) (apply *)))))
+
+(defn make-tidal-scale [scale-data]
+  (let [scale (scale-data :scale)
         freqs (map (comp conv/ratio->cents :bounded-ratio) scale)]
     (str "[" (->> (map #(utils/round2 2 (- % (first freqs))) freqs)
                   (str/join ","))
          "]")))
 (:scale (cps/make 3 [1 3 5 7]))
-(defn make-scala-file [set-size generators period]
-  (let [gens (parse-generators generators)
-        scale-data (cps/make (int set-size) gens :period period
-                             :norm-gen (->> gens (take-last (int set-size)) (apply *)))]
-    (scl/make-scl-file scale-data)))
+
+(defn make-scala-file [scale-data] (scl/make-scl-file scale-data))
+
+(def ONE (e/native->integer 1))
+(defn float->ratio [num]
+  (let [total-decimals (-> (str num)
+                           (str/split ".")
+                           second
+                           count)
+        tens (Math/pow 10 total-decimals)]
+    (e// (e/native->integer (* num tens)) (e/native->integer tens))))
+
+(defn to-e-number [num]
+  (if (= (int num) num)
+    (e/native->integer num)
+    (float->ratio num)))
+
+
+(defn within-bounding-period
+  "Transposes a ratio withing a bounding-period.
+  The octave is a `bounding-period` of 2,the tritave of 3, etc."
+  [bounding-period ratio]
+  {:pre [(> bounding-period 1)]}
+  (let [bounding-period (to-e-number bounding-period)
+        ratio (to-e-number ratio)
+        ratio*  (loop [r ratio]
+                  (cond
+                    (e/> r bounding-period) (recur (e// r bounding-period))
+                    (e/< r ONE) (recur (e/* r bounding-period))
+                    (= bounding-period r) ONE
+                    :else r))]
+    (if (e/ratio? ratio*)
+      (str (e/integer->string (e/numerator ratio*)) "/"
+           (e/integer->string (e/denominator ratio*)))
+      (str (e/integer->string ratio*) "/1"))))
+
+(do
+  (defn scale-table [scale]
+    [:table {:border 1}
+     [:thead
+      [:tr
+       [:th "Degree"]
+       [:th "Cents"]
+       [:th "Ratio"]
+       [:th "Factors"]]]
+     [:tbody
+      (map-indexed
+       (fn [i {:keys [set ratio bounded-ratio bounding-period]}]
+         [:tr {:key set}
+          [:td (inc i)]
+          [:td (utils/round2 4 (conv/ratio->cents bounded-ratio))]
+          [:td (str (within-bounding-period bounding-period (apply * set)))]
+          [:td (str/join ", " set)]
+          ]) scale)]])
+  (scale-table (:scale (cps/make 3 [1 3 5 7]))))
 
 (defn main [state]
   [:div
@@ -62,10 +116,13 @@
      (if (and (> (count generators) 0)
               (> (count set-size) 0)
               (> period 1))
-       (let [{:keys [content filename]} (make-scala-file set-size generators period)]
+       (let [scale-data (get-scale-data set-size generators period)
+             {:keys [content filename]} (make-scala-file scale-data)]
          [:div
+          (scale-table (:scale scale-data))
           [:div [:h3 "MIDI tuning (for use with SuperCollider or Tidal Cycles)"]
-           [:pre {:style {:background-color "lightgray"}} (make-tidal-scale set-size generators period) ]]
+           [:pre {:style {:background-color "lightgray"}}
+            (make-tidal-scale scale-data) ]]
           [:div
            [:h3 "Scala file"]
            [:a {:href (str "data:text/plain;charset=utf-8," content)
