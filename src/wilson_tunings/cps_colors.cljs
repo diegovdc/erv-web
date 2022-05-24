eca863dbd7a2df0cf280d77743f7a2428e0aa3d0
Diego Villaseñor <diego.vid.eco@gmail.com>
2022-05-23 21:38:45 -0500

    [Add] randomized colors and color blend mode selection

--- src/wilson_tunings/cps_colors.cljs
+++ src/wilson_tunings/cps_colors.cljs
@@ -19,6 +19,12 @@
                           (map (fn [k] [k {"r" 0 "g" 0 "b" 0}]))
                           (into (sorted-map)))))
 
+(comment
+  
+  (-> @color-atom)
+  )
+
+(defonce selected-blend-mode (r/atom "multiply"))
 
 (defn get-color-string [color]
   (gstr/format "rgba(%s, %s,  %s, %s)"
@@ -56,27 +62,42 @@
   (-> @pickers)
   (swap! pickers update-in [:a :show?] (comp not boolean)))
 
+(def blend-modes
+  {"normal" (.-normal blend)
+   "multiply" (.-multiply blend)
+   "screen" (.-screen blend)
+   "overlay" (.-overlay blend)
+   "darken" (.-darken blend)
+   "lighten" (.-lighten blend)
+   "colorDodge" (.-colorDodge blend)
+   "colorBurn" (.-colorBurn blend)
+   "hardLight" (.-hardLight blend)
+   "softLight" (.-softLight blend)
+   "difference" (.-difference blend)
+   "exclusion" (.-exclusion blend)
+   "hue" (.-hue blend)
+   "saturation" (.-saturation blend)
+   "color" (.-color blend)
+   "luminosity" (.-luminosity blend)})
+(comment
+  (blend-modes @selected-blend-mode)
+  )
 (defn blend-colors [colors]
   (let [cs (map clj->js colors)]
     (js->clj (reduce
-              (fn [c1 c2] ((.-difference blend) c1 c2))
-              (first cs)
-              (rest cs)))))
-
-#_(blend-colors '({"r" 138, "g" 67, "b" 67, "a" 1}
-                  {"r" 171, "g" 89, "b" 89, "a" 1}))
+               (fn [c1 c2] ((blend-modes @selected-blend-mode) c1 c2))
+               (first cs)
+               (rest cs)))))
 
 (do
   (defn combinations []
-    (let [factors (map first (subvec (vec (seq @color-atom))
-                                     0
-                                     (@cps-config :num-factors)))
+    (let [factors (map first (take (@cps-config :num-factors) (sort-by first (seq @color-atom))))
           groups (map sort (cps/->cps (dec (@cps-config :size)) factors))
           combos (map sort (cps/->cps (@cps-config :size) factors))
           product-squares (->> combos
                                (map (fn [factors]
                                       [(set factors)
-                                       [:div {:style {:margin-left 10}}
+                                       [:div {:key factors :style {:margin-left 10}}
                                         [:h3 (map name factors)]
                                         (->> factors
                                              (select-keys @color-atom)
@@ -86,15 +107,20 @@
                                (into {}))]
       (map
        (fn [g]
-         [:div {:style {:display "flex"}}
+         [:div {:key g :style {:display "flex"}}
           [:h2 (map name g)]
           (map second
                (filter (fn [[fs _]]
                          (set/subset? g fs))
                        product-squares))])
-       groups)))
+       (sort-by first groups))))
 
   (combinations))
+
+(comment
+  (-> @selected-blend-mode)
+  (-> blend-modes keys rand-nth )
+  )
 (defn main [state]
   [:div
    [:h1 "Combination Product Sets Color Maker"]
@@ -114,28 +140,50 @@
                :type :number
                :value (@cps-config :num-factors)
                :on-change #(swap! cps-config assoc :num-factors
-                                  (-> % .-target .-value js/Number))} ]]]]
+                                  (-> % .-target .-value js/Number))}]]]
+
+    [:div
+     [:lable "Color blend mode"]
+     [:select {:value @selected-blend-mode
+               :on-change (fn [v] (reset! selected-blend-mode (.. v -target -value)))}
+      (map (fn [[mode]]
+             [:option {:key mode :value mode} mode])
+           blend-modes)]]
+    [:div
+     [:button {:on-click (fn [_]
+                           (swap! color-atom
+                                  #(into {}
+                                         (map (fn [[k _]]
+                                                [k {"r" (rand-int 256)
+                                                    "g" (rand-int 256)
+                                                    "b" (rand-int 256)
+                                                    "a" 1}])
+                                              %))))}
+      "Randomize colors"]
+     [:button {:on-click (fn [_]
+                           (reset! selected-blend-mode
+                                   (-> blend-modes keys rand-nth)))}
+      "Randomize blend mode"]]]
+
    [:div {:style {:display "flex"
                   :flex-wrap "wrap"}}
     (doall
-     (map
-      (fn [[k color]]
-        (let [on-click #(swap! pickers update-in [k :show?] (comp not boolean)) ]
-          [:div
-           [:h2 (name k)]
-           [:div {:position "relative"}
-            (square color
-                    :on-click on-click)
-            [:div {:position "absolute"}
-             (if (-> @pickers k :show?)
-               [:div (color-picker k color)
-                [:button {:on-click on-click}
-                 "Hide"]]
-               [:button {:on-click on-click}
-                "Change"])]]]))
-      (subvec (vec (seq @color-atom))
-              0
-              (@cps-config :num-factors))))]
+      (map
+        (fn [[k color]]
+          (let [on-click #(swap! pickers update-in [k :show?] (comp not boolean)) ]
+            [:div {:key (name k)}
+             [:h2 (name k)]
+             [:div {:position "relative"}
+              (square color
+                      :on-click on-click)
+              [:div {:position "absolute"}
+               (if (-> @pickers k :show?)
+                 [:div (color-picker k color)
+                  [:button {:on-click on-click}
+                   "Hide"]]
+                 [:button {:on-click on-click}
+                  "Change"])]]]))
+        (take (@cps-config :num-factors) (sort-by first (seq @color-atom)))))]
    [:div {:style {:display "flex"
                   :flex-wrap "wrap"
                   :gap 20}}
