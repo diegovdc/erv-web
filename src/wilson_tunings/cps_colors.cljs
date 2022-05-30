117de9de49b0d114d994c1ea6184511a1356a415
Diego Villaseñor <diego.vid.eco@gmail.com>
2022-05-29 21:52:10 -0500

    Serveral improvements to the cps colors maker

--- src/wilson_tunings/cps_colors.cljs
+++ src/wilson_tunings/cps_colors.cljs
@@ -3,24 +3,23 @@
             [goog.string.format]
             ["react-color" :refer [ChromePicker]]
             ["color-blend" :as blend]
+            ["rgb2hex" :as rgb2hex]
             [erv.cps.core :as cps]
             [reagent.core :as r]
-            [clojure.set :as set]))
-
+            [clojure.set :as set]
+            [clojure.string :as str]))
 
 (defn factor-names [total-factors]
   (map (comp keyword js/String.fromCharCode)
        (range 97 (+ 97 total-factors))))
-
-
 (defonce color-atom (r/atom
                      (->> 26
                           factor-names
                           (map (fn [k] [k {"r" 0 "g" 0 "b" 0}]))
                           (into (sorted-map)))))
 
+
 (comment
-  
   (-> @color-atom)
   )
 
@@ -33,6 +32,9 @@
                (get color "b")
                (get color "a" 1)))
 
+(defn rgb->hex [color]
+  (rgb2hex (get-color-string color)))
+
 (defn color-picker [id color]
   [:div
    [:> ChromePicker
@@ -44,13 +46,11 @@
 
 (defn square [color &
               {:keys [on-click]
-               :or {on-click (fn [_]
-                               (println "cl")
-                               nil)}}]
+               :or {on-click (fn [_] nil)}}]
   (when color
     [:div
-     {:style {:height 30
-              :width 30
+     {:style {:height 50
+              :width 50
               :background-color (get-color-string color)}
       :on-click on-click}]))
 
@@ -88,103 +88,112 @@
                (fn [c1 c2] ((blend-modes @selected-blend-mode) c1 c2))
                (first cs)
                (rest cs)))))
+(reverse (cps/archi-subcps-sets (@cps-config :size) 6))
 
-(do
-  (defn combinations []
-    (let [factors (map first (take (@cps-config :num-factors) (sort-by first (seq @color-atom))))
-          groups (map sort (cps/->cps (dec (@cps-config :size)) factors))
-          combos (map sort (cps/->cps (@cps-config :size) factors))
-          product-squares (->> combos
-                               (map (fn [factors]
-                                      [(set factors)
-                                       [:div {:key factors :style {:margin-left 10}}
-                                        [:h3 (map name factors)]
-                                        (->> factors
-                                             (select-keys @color-atom)
-                                             vals
-                                             blend-colors
-                                             square)]]))
-                               (into {}))]
-      (map
-       (fn [g]
-         [:div {:key g :style {:display "flex"}}
-          [:h2 (map name g)]
-          (map second
-               (filter (fn [[fs _]]
-                         (set/subset? g fs))
-                       product-squares))])
-       (sort-by first groups))))
+(defn combinations [factors combos]
+  (let [groups (map sort (cps/->cps (dec (@cps-config :size)) factors))
+        archi-sets (reverse (cps/archi-subcps-sets (@cps-config :size) (count factors)))
+        product-squares (->> combos
+                             (map
+                               (fn [factors]
+                                 (let [color-data (->> factors
+                                                       (select-keys @color-atom)
+                                                       vals
+                                                       blend-colors)]
+                                   [(set factors)
+                                    {:html
+                                     [:div {:key factors :style {:margin-left 10}}
+                                      [:h3 (map name factors)]
+                                      (square color-data)]
+                                     :color-data color-data}])))
+                             (into {}))]
+    (->> archi-sets
+         (remove (fn [set]
+                   (str/includes? (:name set)
+                                  "1)1")))
+         (map
+           (fn [{:keys [name set]}]
+             [:div {:key name :style {:display "flex"}}
+              [:h2 name]
+              (->> set
+                   (map (fn [factors]
+                          (:html (product-squares factors)))))])))))
 
-  (combinations))
 
-(comment
-  (-> @selected-blend-mode)
-  (-> blend-modes keys rand-nth )
-  )
 (defn main [state]
-  [:div
-   [:h1 "Combination Product Sets Color Maker"]
-   [:div
+  (let [factors
+        (map first (take (@cps-config :num-factors) (sort-by first (seq @color-atom))))
+        combos (map sort (cps/->cps (@cps-config :size) factors))
+        color-data (map (fn [factors*]
+                          [(set factors*)
+                           (->> factors*
+                                (select-keys @color-atom)
+                                vals
+                                blend-colors)])
+                        combos)]
     [:div
-     [:label "Set size "
-      [:input
-       {:min 1
-        :type :number
-        :value (@cps-config :size)
-        :on-change #(swap! cps-config assoc :size
-                           (-> % .-target .-value js/Number))}]]]
-    [:div
-     [:label "Number of factors "
-      [:input {:min 1
-               :max 26
-               :type :number
-               :value (@cps-config :num-factors)
-               :on-change #(swap! cps-config assoc :num-factors
-                                  (-> % .-target .-value js/Number))}]]]
+     [:h1 "Combination Product Sets Color Maker"]
+     [:div
+      [:div
+       [:label "Set size "
+        [:input
+         {:min 1
+          :type :number
+          :value (@cps-config :size)
+          :on-change #(swap! cps-config assoc :size
+                             (-> % .-target .-value js/Number))}]]]
+      [:div
+       [:label "Number of factors "
+        [:input {:min 1
+                 :max 26
+                 :type :number
+                 :value (@cps-config :num-factors)
+                 :on-change #(swap! cps-config assoc :num-factors
+                                    (-> % .-target .-value js/Number))}]]]
 
-    [:div
-     [:lable "Color blend mode"]
-     [:select {:value @selected-blend-mode
-               :on-change (fn [v] (reset! selected-blend-mode (.. v -target -value)))}
-      (map (fn [[mode]]
-             [:option {:key mode :value mode} mode])
-           blend-modes)]]
-    [:div
-     [:button {:on-click (fn [_]
-                           (swap! color-atom
-                                  #(into {}
-                                         (map (fn [[k _]]
-                                                [k {"r" (rand-int 256)
-                                                    "g" (rand-int 256)
-                                                    "b" (rand-int 256)
-                                                    "a" 1}])
-                                              %))))}
-      "Randomize colors"]
-     [:button {:on-click (fn [_]
-                           (reset! selected-blend-mode
-                                   (-> blend-modes keys rand-nth)))}
-      "Randomize blend mode"]]]
+      [:div
+       [:lable "Color blend mode"]
+       [:select {:value @selected-blend-mode
+                 :on-change (fn [v] (reset! selected-blend-mode (.. v -target -value)))}
+        (map (fn [[mode]]
+               [:option {:key mode :value mode} mode])
+             blend-modes)]]
+      [:div
+       [:button {:on-click (fn [_]
+                             (swap! color-atom
+                                    #(into {}
+                                           (map (fn [[k _]]
+                                                  [k {"r" (rand-int 256)
+                                                      "g" (rand-int 256)
+                                                      "b" (rand-int 256)
+                                                      "a" 1}])
+                                                %))))}
+        "Randomize colors"]
+       [:button {:on-click (fn [_]
+                             (reset! selected-blend-mode
+                                     (-> blend-modes keys rand-nth)))}
+        "Randomize blend mode"]]]
 
-   [:div {:style {:display "flex"
-                  :flex-wrap "wrap"}}
-    (doall
-      (map
-        (fn [[k color]]
-          (let [on-click #(swap! pickers update-in [k :show?] (comp not boolean)) ]
-            [:div {:key (name k)}
-             [:h2 (name k)]
-             [:div {:position "relative"}
-              (square color
-                      :on-click on-click)
-              [:div {:position "absolute"}
-               (if (-> @pickers k :show?)
-                 [:div (color-picker k color)
-                  [:button {:on-click on-click}
-                   "Hide"]]
-                 [:button {:on-click on-click}
-                  "Change"])]]]))
-        (take (@cps-config :num-factors) (sort-by first (seq @color-atom)))))]
-   [:div {:style {:display "flex"
-                  :flex-wrap "wrap"
-                  :gap 20}}
-    (combinations)]])
+     [:div {:style {:display "flex"
+                    :flex-wrap "wrap"}}
+      (doall
+        (map
+          (fn [[k color]]
+            (let [on-click #(swap! pickers update-in [k :show?] (comp not boolean)) ]
+              [:div {:key (name k)}
+               [:h2 (name k)]
+               [:div {:position "relative"}
+                (square color
+                        :on-click on-click)
+                [:div {:position "absolute"}
+                 (if (-> @pickers k :show?)
+                   [:div (color-picker k color)
+                    [:button {:on-click on-click}
+                     "Hide"]]
+                   [:button {:on-click on-click}
+                    "Change"])]]]))
+          (take (@cps-config :num-factors) (sort-by first (seq @color-atom)))))]
+     [:div (combinations factors combos)]
+     [:div (map (fn [[fs color-data]]
+                    [:p (str fs " \"" (.-hex (rgb->hex color-data)) "\"")])
+                color-data)]]))
